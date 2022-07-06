65a086f439a1d812555d366caca5f6f3f3da4a29
dongaa <dchoi1211@gmail.com>
2022-07-06 09:01:07 +0900

    adminqa commut;

---
+++ oh-camping/src/main/java/com/pro/cda/AdminQaDAOimpl.java
@@ -0,0 +1,78 @@
+package com.pro.cda;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.pro.yuna.PageDTO;
+
+@Repository
+public class AdminQaDAOimpl implements AdminQaDAO{
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Override
+	public List<AdminQaDTO> getQaList(PageDTO dto) {
+		
+		return this.sqlSession.selectList("adminQaGetList", dto);
+		
+	}
+
+	
+	@Override
+	public int getqacount() {
+		
+		return this.sqlSession.selectOne("adminqagetcount");
+	}
+
+
+	@Override
+	public AdminQaDTO getqacont(int no) {
+		
+		return this.sqlSession.selectOne("admingetcont", no);
+	}
+
+	
+
+	@Override
+	public int adminqaupcount(int no) {
+		
+		return this.sqlSession.update("adminqaupcount", no);
+	}
+
+
+	@Override
+	public int adminqawrite(AdminQaDTO dto) {
+		// TODO Auto-generated method stub
+		return this.sqlSession.insert("adminqawrite", dto);
+	}
+
+
+	@Override
+	public int adminqaupdate(AdminQaDTO dto) {
+		// TODO Auto-generated method stub
+		return this.sqlSession.update("adminqaupdate", dto);
+	}
+
+
+	@Override
+	public int adminqadelete(int no) {
+		// TODO Auto-generated method stub
+		return this.sqlSession.delete("adminqadelete",no);
+	}
+
+
+	@Override
+	public int adminqareplydelte(int no) {
+		// TODO Auto-generated method stub
+		return this.sqlSession.delete("adminqareplydelete", no);
+	}
+	
+	
+	
+	
+	
+}
